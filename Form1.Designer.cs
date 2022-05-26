eeaf80379b2d82d585989badd7ae81760b6086df
eeddgg <39605643+eeddgg@users.noreply.github.com>
2022-05-26 16:29:48 -0700

    Centered text box

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,10 +40,10 @@ namespace WindowsFormsApp1
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button1.Location = new System.Drawing.Point(134, 129);
+            this.button1.Location = new System.Drawing.Point(179, 159);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 19);
+            this.button1.Size = new System.Drawing.Size(112, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Close window";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,14 +53,13 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoEllipsis = true;
             this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 75);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.Size = new System.Drawing.Size(164, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "This is a Dialog Box.";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
@@ -74,21 +73,21 @@ namespace WindowsFormsApp1
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(367, 203);
+            this.panel1.Size = new System.Drawing.Size(489, 250);
             this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 203);
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(489, 250);
             this.Controls.Add(this.panel1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Test";
             this.Load += new System.EventHandler(this.Form1_Load);
